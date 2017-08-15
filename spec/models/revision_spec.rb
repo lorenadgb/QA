07e2e951d56edb255b2ad4fbe1f46632bbdb772b
Lorena Bento <lorenadgb@gmail.com>
2017-08-15 00:51:27 -0300

    Can not change the status of an approved question

--- spec/models/revision_spec.rb
+++ spec/models/revision_spec.rb
@@ -11,6 +11,7 @@ RSpec.describe Revision, type: :model do
   context "Validations" do
     before do
       allow(subject).to receive(:reviewer).and_return admin
+      allow(subject).to receive(:can_change_the_status?).and_return true
     end
 
     let :admin do
@@ -19,6 +20,14 @@ RSpec.describe Revision, type: :model do
 
     let :visitor do
       double(:reviewer, admin?: false)
+    end
+
+    let :approved_question do
+      double(:question, status: QuestionStatus::APPROVED)
+    end
+
+    let :reproved_question do
+      double(:question, status: QuestionStatus::REPROVED)
     end
 
     it 'admin-users can create a revision' do
@@ -47,6 +56,33 @@ RSpec.describe Revision, type: :model do
 
     it 'comment can be blank if revision status is approved' do
       subject.status = RevisionStatus::APPROVED
+
+      subject.save
+
+      expect(subject.errors.full_messages).to eq []
+    end
+
+    it 'Can not change the status of an approved question' do
+      allow(subject).to receive(:can_change_the_status?).and_call_original
+      allow(subject).to receive(:question).and_return approved_question
+
+      subject.comment = 'Fix the question!'
+
+      subject.send(:can_change_the_status?)
+
+      subject.save
+
+      expect(subject.errors[:base]).to eq ['Can not change status of an approved question']
+    end
+
+    it 'Can change the status of a not approved question' do
+      allow(subject).to receive(:can_change_the_status?).and_call_original
+      allow(subject).to receive(:question).and_return reproved_question
+
+      subject.comment = 'Fix the question!'
+      subject.status = QuestionStatus::APPROVED
+
+      subject.send(:can_change_the_status?)
 
       subject.save
 
