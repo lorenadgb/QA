b85df577f3b7df94542486409055ae36ec046acd
Lorena Bento <lorenadgb@gmail.com>
2017-08-18 12:04:49 -0300

    Display truncated text on hover

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -15,4 +15,16 @@ module ApplicationHelper
     end
   end
 
+  def truncate_with_hover(text, options = {})
+    return if text.blank?
+
+    options.reverse_merge!(length: 25)
+
+    if text.length > options[:length]
+      content_tag :span, truncate(text, options), title: text
+    else
+      text
+    end
+  end
+
 end
